259d08811b9e8cef74e4d73abf1ff7c2bf5a4b01
Akshay Mundra <geniusakki27@gmail.com>
2024-02-20 09:37:44 +0530

    added path aliases and custom button component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Style from "./Home.module.css";
+import CustomButton from '@/components/CustomButton/CustomButton';
 
 const Home = () => {
     return (
         <div className={Style.outer_container}>
             <div>
-
+                <CustomButton
+                    color='primary'
+                    variant='contained'
+                >
+                    Hello
+                </CustomButton>
             </div>
         </div>
     )
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components")}`,
+      services: `${path.resolve(__dirname, "./src/services")}`,
+      styles: `${path.resolve(__dirname, "./src/styles")}`,
+      util: `${path.resolve(__dirname, "./src/util")}`,
+    }
+  }
 })
