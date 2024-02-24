5f3cfc38c329167bda4bcb26245858f9f16d6ab4
Akshay Mundra <geniusakki27@gmail.com>
2024-02-24 18:37:08 +0530

    added select search and constant symptom list

--- client/src/components/Layouts/ProtectedLayout/ProtectedLayout.jsx
+++ client/src/components/Layouts/ProtectedLayout/ProtectedLayout.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Style from "./ProtectedLayout.module.css";
 import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectedLayout = () => {
-    const isAuth = null;
+    const isAuth = true;
 
     if (!isAuth) {
         return <Navigate to={'login'} replace />
     }
 
     return (
-        <div>
+        <div className={`${Style.container}`}>
             this is protected layout
             <Outlet />
         </div>
--- client/src/routesMap.js
+++ client/src/routesMap.js
@@ -1,6 +1,8 @@
 import Login from "./pages/authentication/login/Login"
 import Register from "./pages/authentication/register/Register"
 import { Home } from "./pages/home/Home"
+import Dashboard from "./pages/patient/dashboard/Dashboard"
+import DocSearch from "./pages/patient/docSearch/DocSearch"
 
 export const routesMap = {
     '/': {
@@ -24,10 +26,32 @@ export const routesMap = {
 }
 
 export const protectedRoutesMap = {
-    '/user': {
+    // routes for patient 
+    '/dashboard': {
         protected: true,
-        label: 'user',
-        path: '/user',
+        label: 'Dashboard',
+        path: '/dashboard',
+        component: Dashboard
+    },
+    '/search-doctor': {
+        protected: true,
+        label: 'Search Doctor',
+        path: '/search-doctor',
+        component: DocSearch
+    },
+    '/doc-list/:disease': {
+        protected: true,
+        label: 'Doctor List',
+        path: '/doc-list/:disease',
         component: Home
-    }
+    },
+    '/settings': {
+        protected: true,
+        label: 'Settings',
+        path: '/settings',
+        component: Home
+    },
+
+
+    // route for doctor 
 }
